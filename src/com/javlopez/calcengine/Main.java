8d4eff67964718fbb45e654148643e1e3d66fc9f
javier Lopez <sjavierlopez@gmail.com>
2016-10-24 23:22:26 -0500

    Kata: final, Wrappers (StringBuilder), static, toString()

--- src/com/javlopez/calcengine/Main.java
+++ src/com/javlopez/calcengine/Main.java
@@ -4,6 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        String[] statements = {
+                "divide 100.0 50.0",
+                "add 25.0 92.0",
+                "subtract 225.0 17.0",
+                "multiply 11.0 3.0"
+        };
+
+        CalculateHelper helper = new CalculateHelper();
+
+        for(String statement:statements) {
+            helper.process(statement);
+            System.out.println(helper);
+        }
+    }
+    private static void output(String message){
+        System.out.println();
+        System.out.print(message);
+        System.out.println();
+    }
+
+/*
         MathEquation[]  equations = new MathEquation[4];
 
         equations[0] = new MathEquation('d', 100.0d, 50.0d);
@@ -64,5 +86,5 @@ public class Main {
         System.out.println();
         System.out.print(message);
         System.out.println();
-    }
+    }*/
 }
---
+++ src/com/javlopez/calcengine/MathCommand.java
@@ -0,0 +1,11 @@
+package com.javlopez.calcengine;
+
+/**
+ * Created by jlopez on 10/24/16.
+ */
+public enum MathCommand {
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
